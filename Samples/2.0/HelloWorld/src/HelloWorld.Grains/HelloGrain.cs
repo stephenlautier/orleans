047272931bbc7671b19287e3ab49d92a01872df2
Stephen Lautier <stephen.lautier@outlook.com>
2018-03-05 22:36:39 +0100

    get sample working + editorconfig

--- Samples/2.0/HelloWorld/src/HelloWorld.Grains/HelloGrain.cs
+++ Samples/2.0/HelloWorld/src/HelloWorld.Grains/HelloGrain.cs
@@ -1,6 +1,7 @@
 using HelloWorld.Interfaces;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
+using Orleans;
 
 namespace HelloWorld.Grains
 {
@@ -19,7 +20,8 @@ namespace HelloWorld.Grains
         Task<string> IHello.SayHello(string greeting)
         {
             logger.LogInformation($"SayHello message received: greeting = '{greeting}'");
-            return Task.FromResult($"You said: '{greeting}', I say: Hello!");
+	        var key = this.GetPrimaryKey();
+            return Task.FromResult($"You said: '{greeting}', I say: Hello! {key}");
         }
     }
 }
